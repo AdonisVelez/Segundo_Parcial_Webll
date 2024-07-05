868a346ee25da3b3a693e4b666c679c18819caa0
AdonisVelez <aavc.1316765427@gmail.com>
2024-07-04 23:54:39 -0500

    practica8

---
+++ practica8/src/registro/dto/create-registro.dto.ts
@@ -0,0 +1,8 @@
+export class CreateRegistroDto {
+    id: number;
+    fecha: string;
+    hora: string;
+    ubicacion: string;
+    personaId: number;
+    encuestaId: number;
+}
---
+++ practica8/src/registro/entities/registro.entity.ts
@@ -0,0 +1,8 @@
+export class Registro {
+    id: number;
+    fecha: string;
+    hora: string;
+    ubicacion: string;
+    personaId: number;
+    encuestaId: number;
+}
